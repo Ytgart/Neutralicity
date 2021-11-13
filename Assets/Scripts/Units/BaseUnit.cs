eb7dce126ac8e1b5ea1bb5622366477e4d2c33d1
GatariGSPDN4 <gatarigspdn@mail.ru>
2021-11-13 15:04:15 +0300

    BaseUnit

--- Assets/Scripts/Units/BaseUnit.cs
+++ Assets/Scripts/Units/BaseUnit.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseUnit : MonoBehaviour
+public class BaseUnit : MonoBehaviour, IDamageable
 {
-    private float _speed;
+    protected List<Vector2> _path;
+    protected int _index = 0;
+    protected float _speed;
+    protected int _damage;
+    public int HealthPoints { get; set; }
 
-    public void MoveToPosition(Vector3 newPosition)
+    public BaseUnit(List<Vector2> path)
+    {
+        _path = path;
+        transform.position = path[_index++];
+    }
+
+    protected void FindNextDestination()
+    {
+        MoveToPosition(_path[_index++]);
+    }
+    protected void MoveToPosition(Vector2 newPosition)
     {
         transform.position = newPosition;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        HealthPoints -= damageAmount;
     }
 }
